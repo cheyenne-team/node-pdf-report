ba8961bedfbae58f3978da8e039ae1548adb01be
Cristian Miranda <crism60@gmail.com>
2025-11-21 13:45:40 -0300

    import: initial import of v1.1.6

---
+++ lib/index.d.ts
@@ -0,0 +1,63 @@
+/// <reference types="node" />
+import { PdfOptions } from "./exporters";
+import { ReportData } from "./parsers";
+export * from "./exporters";
+export * from "./parsers";
+export interface ReportOptions {
+    title: string;
+    useChartJs: boolean;
+    pdfOptions?: PdfOptions;
+    /**
+     * template file path
+     */
+    template?: string;
+    /**
+     * template raw text
+     */
+    rawTemplate?: string;
+    /**
+     * template file path
+     */
+    footerTemplate?: string;
+    /**
+     * template file path
+     */
+    rawFooterTemplate?: string;
+    /**
+     * template file path
+     */
+    headerTemplate?: string;
+    /**
+     * template raw text
+     */
+    rawHeaderTemplate?: string;
+    /**
+     * style files path
+     */
+    styles?: string[];
+    /**
+     * style raw text
+     */
+    rawStyles?: string[];
+    /**
+     * script files path
+     */
+    scripts?: string[];
+    /**
+     * script raw text
+     */
+    rawScripts?: string[];
+    data: ReportData;
+}
+export declare class HtmlReport {
+    private _parser;
+    createPdf(options: ReportOptions): Promise<Buffer>;
+    release(): void;
+    _$loadReportTemplate(data: any, absoluteTemplateUrl: any, rawTemplate?: any): Promise<string>;
+    _$loadReportStyles({ styles }: {
+        styles: any;
+    }): Promise<any>;
+    _$loadReportScripts({ scripts }: {
+        scripts: any;
+    }): Promise<any>;
+}
